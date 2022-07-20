644eaf724b5bd53f97fe079ee47278dc13b2be87
bindu29 <46557577+bindu29@users.noreply.github.com>
2022-07-20 17:39:12 +0530

    Update main.tf

--- Monitoring/main.tf
+++ Monitoring/main.tf
@@ -1,1 +1,12 @@
-text
+
+resource "aws_prometheus_workspace" "kpi" {
+  alias = "kpi-${var.environment}-${var.pipeline}-${var.service}-sg"
+
+  tags = {
+        Name = "${var.client}-${var.environment}"
+        Owner = "${var.client}"
+        Environment = "${var.environment}"
+        Type = "${var.pipeline}"
+    }
+}
+
