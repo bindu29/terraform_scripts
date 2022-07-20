0bcc375d57e94aaa33f22797b7bb1204b826a5c4
bindu29 <46557577+bindu29@users.noreply.github.com>
2022-07-20 18:07:35 +0530

    Create variable.tf

---
+++ Monitoring/variable.tf
@@ -0,0 +1,26 @@
+variable "client" {
+
+    description = "Client Name"
+    type = string
+  
+}
+variable "environment" {
+
+    description = "Environment value"
+    type = string
+  
+}
+
+variable "pipeline" {
+
+    description = "Name of the pipeline"
+    type = string
+  
+}
+
+variable "service" {
+
+    description = "Service name which are deploying"
+    type = string
+  
+}
