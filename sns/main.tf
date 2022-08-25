bf15b14a3286ba496dd0b1c3c6d03da6681c61e5
bindu29 <46557577+bindu29@users.noreply.github.com>
2022-08-25 11:51:25 +0530

    Update main.tf

--- sns/main.tf
+++ sns/main.tf
@@ -1,19 +1,13 @@
-provider "aws" {
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
-    region = "eu-west-3"
+resource "aws_sns_topic" "kpi_sns_topic" {
+  name = "${var.client}-${var.environment}"
 }
 
-resource "aws_sns_topic" "my_first_sns_topic" {
-  name = var.sns_name
-}
-
-resource "aws_sns_topic_policy" "my_sns_topic_policy" {
-  arn = aws_sns_topic.my_first_sns_topic.arn
+resource "aws_sns_topic_policy" "kpi_topic_policy" {
+  arn = aws_sns_topic.kpi_sns_topic.arn
   policy = data.aws_iam_policy_document.my_custom_sns_policy_document.json
 }
 
-data "aws_iam_policy_document" "my_custom_sns_policy_document" {
+data "aws_iam_policy_document" "kpi_sns_policy_document" {
   policy_id = "__default_policy_ID"
 
   statement {
@@ -46,7 +40,7 @@ data "aws_iam_policy_document" "my_custom_sns_policy_document" {
     }
 
     resources = [
-      aws_sns_topic.my_first_sns_topic.arn,
+      aws_sns_topic.kpi_sns_topic.arn,
     ]
 
     sid = "__default_statement_ID"
