eb7f1d236648bb7bd87c578149afeb154b419f25
bindu29 <46557577+bindu29@users.noreply.github.com>
2022-08-31 18:14:23 +0530

    Create main.tf

---
+++ cloudwatch_sns/main.tf
@@ -0,0 +1,50 @@
+resource "aws_sns_topic" "kpi" {
+  name = "${var.client}-${var.environment}"
+}
+
+resource "aws_sns_topic_policy" "kpi" {
+  arn = aws_sns_topic.kpi.arn
+  policy = data.aws_iam_policy_document.my_custom_sns_policy_document.json
+}
+
+data "aws_iam_policy_document" "kpi" {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        var.account_id,
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      aws_sns_topic.kpi.arn,
+    ]
+
+    sid = "__default_statement_ID"
+  }
+}
+
+
