9bcf0ad929179434a22297bba403a15670bf3df7
bindu29 <46557577+bindu29@users.noreply.github.com>
2022-09-06 12:19:26 +0530

    Create variable.tf

---
+++ cloud_watch/variable.tf
@@ -0,0 +1,52 @@
+variable "region" {
+
+    description = "Region of AWS Service"
+    type = string
+}
+
+variable "client" {
+
+    description = "Client Name"
+    type = string
+  
+}
+variable "environment" {
+
+    description = "Environment value"
+    type = string
+  
+}
+
+variable "pipeline" {
+
+    description = "Name of the pipeline"
+    type = string
+  
+}
+
+variable "service" {
+
+    description = "Service name which are deploying"
+    type = string
+  
+}
+variable "account_id" {
+
+    description = "account id of AWS Service"
+    type = string
+}
+variable "kafka_clustername" {
+
+    description = "Kafka Cluster Name"
+    type = string
+}
+variable "es_domainname" {
+
+    description = "Elastic Search Domain name"
+    type = string
+}
+variable "es_clientid" {
+
+    description = "Elastic Search Client ID"
+    type = string
+}
